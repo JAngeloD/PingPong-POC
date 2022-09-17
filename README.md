368df3d3fdf01a7ab4e5358bfe494846651e1d33
Angelo Devera <deveraangelo319@gmail.com>
2022-09-16 22:26:33 -0600

    Added readme

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # PingPong-POC
+
+A compilation of code showcasing proof of concepts that would eventually 
+be used to actually re-create Ping Pong using SFML without a tutorial.
+
+Currently WIP.
--- Rendering/src/object.cpp
+++ Rendering/src/object.cpp
@@ -15,7 +15,7 @@ void object::init() {
     texture = new sf::Texture();
     sprite = new sf::Sprite();
 
-    if(!texture->loadFromFile("path")) {
+    if(!texture->loadFromFile("D:\\Programming\\PingPong - POC\\Rendering\\bin\\Debug\\Remilia2.png")) {
         std::cout << "REEEE" << std::endl;
     }
 
